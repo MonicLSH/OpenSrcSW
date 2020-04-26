a68a502888ac26b58c92107a367f5bc4948813a7
monic <song001209@naver.com>
2020-04-26 20:51:54 +0900

    Gugudan Ver.2

---
+++ src/Konkuk_SW/Gugudan.java
@@ -0,0 +1,29 @@
+package Konkuk_SW;
+
+import java.util.Scanner;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int num;
+		System.out.print("출력하고 싶은 단을 입력해주십시오 : ");
+		num = scan.nextInt();
+		if(num == 0) {
+			for(int i = 2; i<10; i++) {
+				for(int j = 1; j<10; j++) {
+					System.out.println(i+" x "+j+" = "+i*j);					
+				}
+				System.out.println();
+			}
+		}else if(num < 0){
+			System.out.println("구구단 출력이 불가능합니다.");
+		}else {
+			for(int i = 1; i<10; i++) {
+				System.out.println(num+" x "+i+" = "+num*i);
+			}
+		}
+	}
+
+}
