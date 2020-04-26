48b4ec7f5e3f723a4eb48516cee0788c934247cf
monic <song001209@naver.com>
2020-04-26 20:53:55 +0900

    Gugudan Ver.1.5

--- src/Konkuk_SW/Gugudan.java
+++ src/Konkuk_SW/Gugudan.java
@@ -10,8 +10,19 @@ public class Gugudan {
 		int num;
 		System.out.print("출력하고 싶은 단을 입력해주십시오 : ");
 		num = scan.nextInt();
-		for(int i = 1; i<10; i++) {
-			System.out.println(num+" x "+i+" = "+num*i);
+		if(num == 0) {
+			for(int i = 2; i<10; i++) {
+				for(int j = 1; j<10; j++) {
+					System.out.println(i+" x "+j+" = "+i*j);					
+				}
+				System.out.println();
+			}
+		}else {
+			for(int i = 1; i<10; i++) {
+				System.out.println(num+" x "+i+" = "+num*i);
+			}
 		}
 	}
+
 }
+
